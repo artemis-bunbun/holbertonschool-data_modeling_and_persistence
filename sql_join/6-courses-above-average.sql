a45c9cfa0f47f0817903b94a29c02352a47d086f
artemis-bunbun <starfruitworld@gmail.com>
2026-06-05 18:06:02 +0200

    fix: syntax: typo

--- sql_join/6-courses-above-average.sql
+++ sql_join/6-courses-above-average.sql
@@ -1,17 +1,21 @@
-SELECT course_title
-FROM (
-    SELECT courses.title AS course_title, COUNT(enrollments.student_id) AS enrollment_count
-    FROM courses
-    LEFT JOIN enrollments ON courses.id = enrollments.course_id
-    GROUP BY courses.id, courses.title
-) AS course_enrollments
-WHERE enrollment_count > (
-    SELECT AVG(enrollment_count)
+SELECT title AS course_title
+FROM courses
+WHERE id IN (
+    SELECT course_id
     FROM (
-        SELECT COUNT(enrollments.student_id) AS enrollment_count
+        SELECT courses.id AS course_id, COUNT(enrollments.student_id) AS enrollment_count
         FROM courses
         LEFT JOIN enrollments ON courses.id = enrollments.course_id
         GROUP BY courses.id
-    ) AS average_enrollments
+    ) AS course_enrollments
+    WHERE enrollment_count > (
+        SELECT AVG(enrollment_count)
+        FROM (
+            SELECT COUNT(enrollments.student_id) AS enrollment_count
+            FROM courses
+            LEFT JOIN enrollments ON courses.id = enrollments.course_id
+            GROUP BY courses.id
+        ) AS average_enrollments
+    )
 )
 ORDER BY course_title ASC;
